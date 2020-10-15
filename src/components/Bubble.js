03998e4483f6f62b47de961537e6eacaa7824802
IrvinTarlao <irvin@millionvictories.com>
2020-10-15 09:24:05 +0200

    fix reactstrap message

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as style from '../styles';
 import Moment from 'react-moment';
+import { Row, Col } from 'reactstrap';
 
 
 const Bubble = ({ message, colorReply }) => {
@@ -26,33 +27,41 @@ const Bubble = ({ message, colorReply }) => {
 
     return (
         <div>
-            <div style={{ width: "100%", marginTop: "20px", display: 'flex', alignItems: "flex-end", justifyContent: "flex-end" }}>
-                {message.attachments.length === 0 && <img className="d-none d-md-block" src={message.avatar} alt="avatar" style={{ borderRadius: "50%", height: "60px", margin: "0 15px 0 10px" }}></img>}
-                <div style={{ width: "80%", padding: "30px", backgroundColor: colorReply || style.bubbleColor, textAlign: 'left', borderRadius: '10px', color: style.nameColor }}>{message.message}</div>
-                <div style={{ marginLeft: "15px", fontSize: '0.8em' }}>
+            <Row style={{ display: 'flex', alignItems: 'flex-end', margin:"5px"}}>
+                <Col xs={1} md={2} style={{ display: 'flex', justifyContent: 'center' }}>
+                    {message.attachments.length === 0 && <img className="d-none d-md-block" src={message.avatar} alt="avatar" style={{ borderRadius: "50%", height: "60px" }}></img>}
+                </Col>
+                <Col xs={10} md={8} style={{ display: 'flex', justifyContent: 'center', fontSize: '0.9em' }}>
+                    <div style={{ width: "100%", padding: "30px", backgroundColor: colorReply || style.bubbleColor, textAlign: 'left', borderRadius: '10px', color: style.nameColor }}>{message.message}</div>
+                </Col>
+                <Col xs={1} md={2} style={{ display: 'flex', justifyContent: 'center', fontSize: '0.8em' }}>
                     <Moment format="HH:MM">
                         {message.time}
                     </Moment>
-                </div>
-            </div>
-            <div style={{ display: 'flex', alignItems: 'center', marginTop: "20px"}}>
-                {message.attachments.length > 0 &&
-                    <div style={{ width: '80%' }}>
-                        <div style={{ width: "100%", display: 'flex', alignItems: "center" }}>
-                            <img className="d-none d-md-block" src={message.avatar} alt="avatar" style={{ borderRadius: "50%", height: "60px", margin: "0px 15px 0 40px" }}></img>
-                            {message.attachments.map((a, i) =>
-                                <div key={'attach' + i} style={attachStyle}>
-                                    <div style={{ backgroundColor: getAttachColor(a), width: "40px", height: "40px", borderRadius: "5px", marginRight: "10px", display: 'flex', justifyContent: 'center', alignItems: 'center', color: style.displayMsgColor, fontWeight: "bold" }}>{getChars(a)[1]}</div>
-                                    <div style={{ textAlign: 'left' }}>
-                                        <div style={{ fontWeight: 'bold' }}>{getChars(a)[0]}</div>
-                                        <div>{getChars(a)[1]}</div>
-                                    </div>
-                                    <i style={{ marginLeft: "30px" }} className="fas fa-download"></i>
-                                </div>)}
-                        </div>
-                    </div>
-                }
-            </div>
+                </Col>
+            </Row>
+            {message.attachments.length > 0 &&
+                <Row style={{ display: 'flex', alignItems: 'flex-end' }}>
+                    <Col xs={1} md={2} style={{  display: 'flex', justifyContent: 'center' }}>
+                        <img className="d-none d-md-block" src={message.avatar} alt="avatar" style={{ borderRadius: "50%", height: "60px" }}></img>
+                    </Col>
+                    <Col xs={10} md={8} style={{ display: 'flex', justifyContent: 'flex-start', fontSize: '0.9em' }}>
+                        {message.attachments.map((a, i) =>
+                            <div key={'attach' + i} style={attachStyle}>
+                                <div style={{ backgroundColor: getAttachColor(a), width: "40px", height: "40px", borderRadius: "5px", marginRight: "10px", display: 'flex', justifyContent: 'center', alignItems: 'center', color: style.displayMsgColor, fontWeight: "bold" }}>{getChars(a)[1]}</div>
+                                <div style={{ textAlign: 'left' }}>
+                                    <div style={{ fontWeight: 'bold' }}>{getChars(a)[0]}</div>
+                                    <div>{getChars(a)[1]}</div>
+                                </div>
+                                <i style={{ marginLeft: "30px" }} className="fas fa-download"></i>
+                            </div>)}
+                    </Col>
+                    <Col xs={1} md={2} style={{ display: 'flex', justifyContent: 'center', fontSize: '0.8em' }}>
+                        
+                    </Col>
+                </Row>
+            }
+            
 
         </div>
 
